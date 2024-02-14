99bcb2bd18873fe2f3b59523baf7faed4fa143b7
seizethedaylyn <rlacofls294@gmail.com>
2024-02-14 13:28:16 +0900

    init

---
+++ src/main/java/com/example/vuebackboard/entity/BoardRepository.java
@@ -0,0 +1,11 @@
+package com.example.vuebackboard.entity;
+
+/* BoardRepository.java */
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+    Page<BoardEntity> findAllByOrderByIdxDesc(Pageable pageable);
+}
---
+++ src/test/java/com/example/vuebackboard/VueBackboardApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.vuebackboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueBackboardApplicationTests {
+
+}
